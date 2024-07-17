187f3c42bb29dcb2c39c77d2c5beda951f4f8c7e
Kabir <sagithedeveloper@gmail.com>
2024-07-17 12:24:16 +0530

    added the code

---
+++ S7-Attributes/flat.js
@@ -0,0 +1,10 @@
+const arr1 = [0, 1, 2, [3, 4]];
+
+console.log(arr1.flat());
+// Expected output: Array [0, 1, 2, 3, 4]
+
+const arr2 = [0, 1, 2, [[[3, 4]]]];
+
+console.log(arr2.flat(3));
+
+// Expected output: Array [0, 1, 2, Array [3, 4]]
