3a3fa7567c9c5617744b630dee1e6ba9cddb29ab
Kabir <sagithedeveloper@gmail.com>
2024-07-30 12:42:10 +0530

    Added the code

---
+++ JS/S04_Data-types/Add.js
@@ -0,0 +1,15 @@
+var num1;
+
+num1 = 100;
+
+var num2;
+
+num2 = 500;
+
+var result;
+
+result = num1 + num2;
+
+console.log(num1, num2);
+
+console.log(result);
---
+++ JS/S04_Data-types/Swap.js
@@ -0,0 +1,7 @@
+var x = 10;
+
+var y = 20;
+
+// writ code to print value of x = 20 and y = 10
+
+console.log(x, y); //20,10
