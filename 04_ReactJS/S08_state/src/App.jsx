dd170f492db4383608adc50db9595e8523d1fbde
Kabir <sagithedeveloper@gmail.com>
2024-10-03 12:42:00 +0530

    added the code

---
+++ 04_ReactJS/S08_state/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Counter from "./components/counter/Counter";
+function App() {
+  return (
+    <div className="container">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ 04_ReactJS/S08_state/src/components/counter/Counter.jsx
@@ -0,0 +1,46 @@
+// import "./Counter.css";
+// import { useState } from "react";
+// function Counter() {
+//   let myContainer = useState(999); //myContainer = [state,setterFn]
+//   var count = 0;
+
+//   function incCount() {
+//     count = count + 1;
+//     console.log(count);
+
+//     // myContainer[0] = myContainer[0] + 1;
+//     // console.log(myContainer[0]);
+
+//     myContainer[1](myContainer[0] + 1);
+//   }
+
+//   return (
+//     <div className="counter">
+//       <h2>Count Value is : {myContainer[0]}</h2>
+
+//       <button onClick={incCount}>incCount</button>
+//     </div>
+//   );
+// }
+
+// export default Counter;
+
+import "./Counter.css";
+import { useState } from "react";
+function Counter() {
+  let arr = useState(0); //arr = [state,setter]
+
+  function incCount() {
+    arr[1](arr[0] + 1);
+  }
+
+  return (
+    <div className="counter">
+      <h2>Count Value is : {arr[0]}</h2>
+
+      <button onClick={incCount}>incCount</button>
+    </div>
+  );
+}
+
+export default Counter;
